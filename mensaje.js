f8ad50e79228342714c990c6a2b53baefe9c0332
Edw4rd19 <127695102+Edw4rd19@users.noreply.github.com>
2024-02-15 02:33:42 -0600

    Delete mensaje.js

--- mensaje.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('carta').addEventListener('click', function() {
-    var message = document.getElementById('carta.jpg');
-    message.style.display = message.style.display === 'none'  'block'  'none';
-});
-
-
